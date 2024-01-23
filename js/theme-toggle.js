55979b8c487359a2665a42530eda945bd4d5cd32
Iskandar-tulkinov <iscandardev@gmail.com>
2024-01-24 02:32:25 +0500

    feat: add thmeToggler, variables, create components dir and form, import icons from icomoon

---
+++ js/theme-toggle.js
@@ -0,0 +1,30 @@
+"use strict";
+const themeToggler = document.getElementById("theme-toggler");
+
+themeToggler.addEventListener("click", ()=>{
+  setTimeout(toggleTheme,100)})
+
+// Retrieve the theme from localStorage on page load
+const savedTheme = localStorage.getItem("theme");
+
+// Set initial theme based on localStorage or default to light theme
+// document.body.classList.toggle("dark-theme", savedTheme === "dark");
+if(savedTheme==="dark"){
+  document.body.classList.toggle("dark-theme");
+  themeToggler.src = "../assets/img/sun.svg";
+}
+
+function toggleTheme() {
+  document.body.classList.toggle("dark-theme");
+
+  // Save the current theme state to localStorage
+  localStorage.setItem("theme", document.body.classList.contains("dark-theme") ? "dark" : "light");
+
+  // Update theme toggler w image based on the new theme
+  if (document.body.classList.contains("dark-theme")) {
+    themeToggler.src = "../assets/img/sun.svg";
+  } else {
+    themeToggler.src = "../assets/img/moon.svg";
+  }
+
+}
